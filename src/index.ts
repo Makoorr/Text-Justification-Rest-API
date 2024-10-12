dd99e1a18738a6dff79008a6ec3a6db0f8c1c335
Makoorr <azizbenrejeb@gmail.com>
2024-10-13 00:30:05 +0100

    Updated vercel config

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import createServer from "./server";
 const app = createServer();
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send('Hello TicTac!');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
